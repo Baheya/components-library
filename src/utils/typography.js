fb26cf2979b914b31b0d5a30a3fa25e52baeb1f8
Baheya <khalifa.baheya@gmail.com>
2020-11-08 14:41:48 +0400

    added type scale

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+export const primaryFont = '"Poppins", sans-serif';
+
+export const typeScale = {
+  h1: '3rem',
+  h2: '2.4rem',
+  h3: '1.9rem',
+  h4: '1.5rem',
+  h5: '1.25rem',
+  paragraph: '1rem',
+  helperText: '0.8rem',
+  copyrightText: '0.6rem',
+};
